f22bc7f4aa43e0d0e9c035555271b99fba26aebc
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-07-29 17:24:36 +0530

    Time: 77 ms (65.79%), Space: 66.4 MB (20.51%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        // Initialize the tortoise and hare pointers.
+
+    // Brute Force Approach -> Time Complexity : O(n^2)
+    int bruteForce(vector<int>&nums) {
+        int n= nums.size();
+        for(int i=0;i<n;i++) {
+            bool isRepeated = false;
+        for(int j=i+1;j<n;j++) {
+            if(nums[i]==nums[j]) {
+                isRepeated = true;
+                return nums[i];
+            }
+        }
+        }
+        return -1;
+    }
+
+    // First Optimized Approach -> Time Complexity : O(n)
+    int HashingOrHashSetApproach(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return num;
+            }
+            seen.insert(num);
+        }
+        return -1;
+    }
+
+    // Second Optimized Approach
+    // Floyd's Tortoise and Hare Algorithm -> Time Complexity : O(n)
+    int FloydTortoiseAndHareAlgo(vector<int>nums) {
         int tortoise = nums[0];
         int hare = nums[0];
         
@@ -20,5 +49,11 @@ public:
         
         // The duplicate number is found.
         return hare;
+    }
+
+    int findDuplicate(vector<int>& nums) {
+    // return bruteForce(nums);
+    // return HashingOrHashSetApproach(nums);
+    return FloydTortoiseAndHareAlgo(nums);
     }
 };
