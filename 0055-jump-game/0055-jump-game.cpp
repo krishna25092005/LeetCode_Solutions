644dbccdabceff27df95d5239e74fea8f34088c2
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-10-04 20:38:37 +0530

    Time: 47 ms (44.01%), Space: 51.2 MB (18.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    bool canJump(vector<int>& nums) {
-        int ans = 0; 
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            
-            if (i > ans) return false;
-            
+    bool canJump(vector<int> &nums)
+    {
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+
+            if (i > ans)
+                return false;
+
             ans = max(ans, i + nums[i]);
-            
+            if (ans >= nums.size() - 1) return true;
         }
-        
+
         return true;
     }
 };
