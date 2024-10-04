c6d559dadf0e5ccb5bd0e79bd89276eb6a8c4c1d
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-10-04 20:36:12 +0530

    Time: 38 ms (83.93%), Space: 51.1 MB (80.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,13 +4,13 @@ public:
         int ans = 0; 
         
         for (int i = 0; i < nums.size(); ++i) {
+            
             if (i > ans) return false;
             
             ans = max(ans, i + nums[i]);
             
-            if (ans >= nums.size() - 1) return true;
         }
         
-        return false;
+        return true;
     }
 };
