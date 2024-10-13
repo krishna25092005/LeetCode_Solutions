28dbcc6bbede8dcc8cde0d2cd0f8c655fdbf561d
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-10-13 20:45:49 +0530

    Time: 4 ms (57%), Space: 14.9 MB (6.5%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        
+        // Solving using STL - One Liner \U0001f60e\U0001f60e
+        // Time Complexity : O(n)
+        // Space Complexity : O(1)
         next_permutation(nums.begin(), nums.end());
     }
 };
