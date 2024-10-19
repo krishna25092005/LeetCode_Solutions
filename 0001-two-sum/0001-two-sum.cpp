0c2a784aed3535d192e8c8b2c89af6e61056005e
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-10-19 20:14:46 +0530

    Time: 3 ms (98.48%), Space: 14.2 MB (43.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        int size = nums.size();
-        vector<pair<int, int>> p;
-
-        // Create a vector of pairs (number, original index)
-        for (int i = 0; i < size; ++i) {
-            p.push_back({nums[i], i});
+    unordered_map<int, int> hash_map;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (hash_map.find(complement) != hash_map.end()) {
+            return {hash_map[complement], i};  // Return the indices
         }
-
-        // Sort the vector of pairs based on the number values
-        sort(p.begin(), p.end());
-
-        int low = 0;
-        int high = size - 1;
-
-        while (low < high) {
-            int currSum = p[low].first + p[high].first;
-            if (currSum == target) {
-                ans.push_back(p[low].second);
-                ans.push_back(p[high].second);
-                return ans;
-            } else if (currSum > target) {
-                high--;
-            } else {
-                low++;
-            }
-        }
-
-        return ans; // return an empty vector if no pair is found
+        hash_map[nums[i]] = i;  // Store the number with its index
     }
+    return {};
+}
+
 };
