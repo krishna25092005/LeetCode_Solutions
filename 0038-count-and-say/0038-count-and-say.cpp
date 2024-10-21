0696a68bae1c7e2a97d9cc08799d6745aab5ef6b
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-10-21 18:27:32 +0530

    Time: 6 ms (46.76%), Space: 10.3 MB (62.81%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string countAndSay(int n,string res="1") {
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.size(); i++) {
+                int c = 1;
+                while ((i + 1 < res.size()) && (res[i] == res[i + 1]))c++,i++;
+                cur += to_string(c) + res[i];
+            }
+            res = cur;
+        }
+        return res;
+    }
+};
