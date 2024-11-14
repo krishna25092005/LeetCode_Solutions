c90b814866157dd33de0f16d4e680138b8f0defa
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-11-14 20:34:44 +0530

    Time: 2 ms (36.65%), Space: 9.7 MB (47.55%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    string decodeMessage(string key, string message)
+    {
+        unordered_map<char, char> mp;
+        char currentChar = 'a';
+
+        for (char ch : key)
+        {
+            if (ch != ' ' && mp.find(ch) == mp.end())
+            {
+                mp[ch] = currentChar;
+                currentChar++;
+            }
+        }
+
+        string decodedMessage;
+        for (char ch : message)
+        {
+            if (ch == ' ')
+            {
+                decodedMessage += ' ';
+            }
+            else
+            {
+                decodedMessage += mp[ch];
+            }
+        }
+
+        return decodedMessage;
+    }
+};
