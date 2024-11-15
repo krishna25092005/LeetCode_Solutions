ebb9ea6f56ee5b31909a9943627acd11c0a4082d
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-11-15 23:24:51 +0530

    Time: 1 ms (34.78%), Space: 9 MB (55.27%) - LeetHub

---
+++ 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    bool canTransform(string s, string e)
+    {
+        int i = 0, j = 0, p = s.size(), q = e.size();
+        while (i < p || j < q)
+        {
+            while (s[i] == 'X')
+                i++;
+            while (e[j] == 'X')
+                j++;
+
+            if (s[i] != e[j])
+                return false;
+            if (s[i] == 'R' && i > j)
+                return false;
+            if (s[i] == 'L' && i < j)
+                return false;
+            i++, j++;
+        }
+        return true;
+    }
+};
