3622e860fece608fd2df2158f4ebe4d18da7cf49
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-11-15 23:25:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (98.4%) - LeetHub

--- 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.cpp
+++ 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.cpp
@@ -6,6 +6,7 @@ public:
         int i = 0, j = 0, p = s.size(), q = e.size();
         while (i < p || j < q)
         {
+            
             while (s[i] == 'X')
                 i++;
             while (e[j] == 'X')
