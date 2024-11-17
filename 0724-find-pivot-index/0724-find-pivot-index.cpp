586882c2969ae204aef36d5af70a719e651c0e99
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-11-17 23:59:42 +0530

    Time: 0 ms (100%), Space: 34.6 MB (68.7%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,48 +1,12 @@
 class Solution {
 public:
-
-    // Brute Force Approach {Time Complexity : O(n^2)}
-    int BruteForce(vector<int>&nums) {
-        int leftSum=0;
-        int rightSum=0;
-    for(int i=0;i<nums.size();i++) {
-
-        // Find leftSum excluding current index
-        for(int j=0;j<i;j++) {
-            leftSum += nums[j];
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = accumulate(nums.begin(),nums.end(),0);
+        int leftSum = 0;
+        for(int i=0;i<nums.size();i++) {
+            if(leftSum==totalSum-leftSum-nums[i]) return i;
+            leftSum += nums[i];
         }
-
-        // Find rightSum excluding current index
-        for(int j=i+1;j<nums.size();j++) {
-            rightSum += nums[j];
-        }
-    if(leftSum==rightSum) return i;
-        }
-    return -1;
+        return -1;
     }
-
-// Prefix Sum - Optimized Approach  {Time Complexity : O(n)}
-    int PrefixSum(vector<int>&nums) {
-    vector<int>leftSum(nums.size(),0);
-    vector<int>rightSum(nums.size(),0);
-    for(int i=1;i<nums.size();i++) {
-        leftSum[i] = leftSum[i-1] + nums[i-1];
-    }
-
-    for(int i=nums.size()-2;i>=0;i--) {
-        rightSum[i] = rightSum[i+1] + nums[i+1];
-    }
-
-    for(int i=0;i<nums.size();i++) {
-        if(leftSum[i]==rightSum[i]) return i;
-    }
-    return -1;
-    }
-
-int pivotIndex(vector<int>& nums) {
-
-// return BruteForce(nums);
-return PrefixSum(nums);
-
-}
 };
