25674d1efeb497ca7db19498fcb413b754457f89
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-12-15 23:00:12 +0530

    Time: 3 ms (61.89%), Space: 7.8 MB (19.76%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+        
+        int prev1 = 1, prev2 = 0;
+        for (int i = 2; i <= n; ++i) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
