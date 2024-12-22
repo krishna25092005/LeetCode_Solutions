2d95f46d23e4ed9cab993ea97eaa2681c5866730
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-12-22 22:26:56 +0530

    Time: 7 ms (33.74%), Space: 12.5 MB (49.01%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        // Define a mapping of integer values to their respective Roman numeral representations.
+        vector<pair<int, string>> romanSymbols = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, 
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+
+        string result;
+        for (const auto& [value, symbol] : romanSymbols) {
+            while (num >= value) {
+                result += symbol;
+                num -= value;
+            }
+        }
+
+        return result;
+    }
+};
